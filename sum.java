33f259e3c1d051e6a788be041e4a4e515b10492b
spshyamaprasad1234 <spshyamaprasad1234@gmail.com>
2023-10-14 23:53:25 +0530

    sum of N nunber without using loop

--- sum.java
+++ sum.java
@@ -1,15 +1,12 @@
 import java.util.*;
 
-public class sum_of_num {
+public class sum {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("N : ");
         int N = scan.nextInt();
 
-        int sum = 0;
-        for(int i = 1;i <= N;i++) {
-            sum = sum + i;
-        }
+        int sum = (N*(N+1))/2;
 
         System.out.println("Sum : " + sum);
 
